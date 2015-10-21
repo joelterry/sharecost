e8c73bd456b244f8a6b2b29eebc70afe92e5f50e
Nathan Chong <nkmchong@gmail.com>
2015-10-20 20:23:11 -0700

    add JQuery and Bootstrap

--- client/ui.js
+++ client/ui.js
@@ -1,1 +1,21 @@
 /* Module for manipulating front end. */
+
+function loadfile(filename, filetype){
+    if (filetype=="js"){ //filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+loadfile("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css", "css");
+loadfile("https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js", "js");
+loadfile("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js", "js");

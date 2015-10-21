b67e6b78a08652b51e0aab7e2a866a4c0d92905f
trent <tjmcc01@gmail.com>
2015-10-20 20:53:06 -0700

    add scope to make payments

--- sharecost.js
+++ sharecost.js
@@ -44,7 +44,7 @@ if (Meteor.isServer) {
     ServiceConfiguration.configurations.insert({
       service: "venmo",
       clientId: "3008",
-      scope: "access_profile+access_friends",
+      scope: "access_profile+access_friends+make_payments",
       secret: "s4CH2SZAwKJuLtFvn7eUyEcJMDr5bcbt"
     });
 

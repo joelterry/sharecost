c3dcda93f6cb5952e59b01169c34128790ac52b5
ashenazure <alex.ma@berkeley.edu>
2015-10-23 11:59:54 -0700

    Tests for checking purchase fields

--- tests/jasmine/client/unit/purchaseDbSpec.js
+++ tests/jasmine/client/unit/purchaseDbSpec.js
@@ -1,11 +1,23 @@
 "use strict";
 describe("Purchase test", function () {
-  it("check data constraints", function () {
-    //Purchases.insert({"id": 1, "title": "Gucci", "description": "Go Bears", "creator": 42, "members": [{"venmo_id": 42, "vote_status": 0}, {"venmo_id": 34, "vote_status": 1}], "cost": 24, "created_at": 10024});
-    
-    var purchase = Purchases.find({"id" : 1})["collection"]["_docs"]["_map"]["ge3tGQZ97TFpy9XX9"] //hacky
+  var myDatabase = Purchases;
+  afterAll(function () {
+    myDatabase.remove({"_id": "1"});
+  });
 
-    expect(purchase["title"]).toBe("Gucci");
-    expect(purchase["title"].length).toBeLessThan(64);
+  it("check data constraints, no error", function () {
+    var entry = {"_id": "1", "title": "Gucci", "description": "Go Bears", "creator": 42, "members": [32], "accepted": [], "rejected": [], "member_names": {32: "Joe"}, "paid": [], "cost": 24, "created_at": 10024};
+    if (Meteor.call("check_purchase", entry) == null) {
+      myDatabase.insert(entry);
+      var purchase = myDatabase.find({"_id" : "1"}).collection._docs._map["1"];
+      expect(purchase.title).toBe("Gucci");
+      expect(purchase.title.length).toBeLessThan(64);
+    }
+    expect(Meteor.call("check_purchase", entry)).toBeUndefined();
+  });
+
+  it("check data constraints, title too short", function () {
+    var entry = {"_id": "1", "title": "", "description": "Go Bears", "creator": 42, "members": [32], "accepted": [], "rejected": [], "member_names": {32: "Joe"}, "paid": [], "cost": 24, "created_at": 10024};
+    expect(Meteor.call("check_purchase", entry)).not.toBeNull();
   });
 });

949e445a1b39fb9adf867285eea938c1b39f1a01
trent <tjmcc01@gmail.com>
2015-11-17 16:39:03 -0800

    moves keys to settings.json

--- server/server.js
+++ server/server.js
@@ -2,23 +2,15 @@
 
 Meteor.startup(function () {
 
-	if (process.env.NODE_ENV === "development"){
-		var clientId = "3008";
-		var secret = "s4CH2SZAwKJuLtFvn7eUyEcJMDr5bcbt";
-	} else {
-		var clientId = "3018";
-		var secret = "YaCkWypcB6AGYd7xjcYmFwVrtVTDybAm";
-	}
-
 	ServiceConfiguration.configurations.remove({
 		service: "venmo"
 	});
 
 	ServiceConfiguration.configurations.insert({
 		service: "venmo",
-		clientId: clientId,
+		clientId: Meteor.settings.VENMO.CLIENT_ID,
 		scope: "access_profile+access_friends+make_payments",
-		secret: secret
+		secret: Meteor.settings.VENMO.SECRET
 	});
 
 });
